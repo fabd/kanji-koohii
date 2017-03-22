e0baf41339ab2e37c7609e09d48b6c6ec75a2c3b
Fabrice Denis <fabrice.denis@gmail.com>
2017-03-22 16:30:02 +0100

    Add config/ folder

---
+++ config/error/error.html.php
@@ -0,0 +1,48 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>Woops!</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="noindex, nofollow" />
+	<link rel="Shortcut Icon" type="image/x-icon" href="/favicon.ico" />
+  <style type="text/css">
+body {
+	font:14px/1.6em Verdana, Geneva, Arial, Helvetica, sans-serif; color:#000;
+	margin:0; padding:0; background:#FDF9CA;
+}	
+a { color:#005CB1; font-weight:normal; text-decoration:underline; }
+a:hover { text-decoration:underline; }
+p, ul { margin:0 0 1em; }
+.clear { clear:both; overflow:hidden; height:0; }
+.app-header { width:100%; border-bottom:1px solid #CCCCCC; position:relative; padding-bottom:8px; margin-bottom:12px; }
+.app-header h2 { line-height:1em; margin:0; font:normal 19px/1em Georgia, serif; color:#101C8F; text-transform:none; letter-spacing:normal; }
+.main {
+	position:relative; padding:20px 20px 1em; width:510px; margin:0 0 0 194px;
+	background:#fffde7; border-right:1px solid #e8e5c9; border-left:1px solid #e8e5c9; border-bottom:1px solid #d1cfb5;
+}
+  </style>
+</head>
+<body>
+
+  <a href="http://kanji.koohii.com" style="position:absolute;left:33px;top:22px;width:131px;"><img src="/images/maint/maintenance-logo.gif" style="border:none;"/></a>
+
+	<div class="main">
+
+		<div class="app-header">
+			<h2>Woops! An Error Occured.</h2>
+			<div class="clear"></div>
+		</div>
+
+		<p> Looks like the website encountered an unfortunate scenario also known as the HTTP 500 Error page!<br/>
+		    <br/>
+				If the problem persists <a href="http://kanji.koohii.com/contact">please let us know</a> so we can fix it!
+    </p>
+
+    <ul>
+			<li>Go to the <a href="http://kanji.koohii.com">Kanji Koohii</a> homepage.</li>
+		</ul>
+
+  </div>
+
+</body>
+</html>

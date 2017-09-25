6d6cb5b681b57d44c7c07b792a838df220a2bdeb
Fabrice Denis <fabrice.denis@gmail.com>
2017-09-25 17:34:12 +0200

    Start moving a lot of styles to root KanjiFlashcard.vue component

--- lib/front/vue/components/KoohiiFlashcardKanji.vue
+++ lib/front/vue/components/KoohiiFlashcardKanji.vue
@@ -1,30 +1,30 @@
 <template>
 
-<div>
+<div class="fc-kanji">
 
   <template v-if="!reviewMode.freemode">
     <a id="uiFcMenu" href="#" title="Edit Flashcard" class="uiGUI uiFcAction"
      data-action="fcmenu" :data-uri="reviewMode.fc_edit_uri" :data-param="reviewMode.fc_edit_params"><i class="fa fa-bars"></i></a>
   </template>
 
-  <div id="keyword" v-html="cardData.keyword"></div>
+  <div class="d-keyword" v-html="cardData.keyword"></div>
 
-  <div id="kanjibig">
+  <div class="d-kanji">
     <p>
       <cjk_lang_ja :html="cardData.kanji"></cjk_lang_ja>
     </p>
   </div>
 
-  <div id="strokecount" title="Stroke count">
+  <div class="d-strokec" title="Stroke count">
     <cjk_lang_ja html="&#30011;&#25968;" className="kanji"></cjk_lang_ja><span>{{ cardData.strokecount }}</span>
   </div>
 
-  <div id="framenum" v-html="cardData.framenum"></div>
+  <div class="d-framenr" v-html="cardData.framenum"></div>
   
-  <div id="uiFcYomi" v-if="reviewMode.fc_yomi">
+  <div class="d-yomi" v-if="reviewMode.fc_yomi">
     <div class="pad">
 
-      <div class="yomi y_o" v-if="cardData.v_on">
+      <div class="y_o" v-if="cardData.v_on">
         <div>
           <cjk_lang_ja className="vyc">{{ cardData.v_on.compound }}</cjk_lang_ja>
           <cjk_lang_ja className="vyr" :html="cardData.v_on.reading"></cjk_lang_ja>
@@ -32,7 +32,7 @@
         <div class="vyg">{{ cardData.v_on.gloss }}</div>
       </div>
       
-      <div class="yomi y_k" v-if="cardData.v_kun">
+      <div class="y_k" v-if="cardData.v_kun">
         <div>
           <cjk_lang_ja className="vyc">{{ cardData.v_kun.compound }}</cjk_lang_ja>
           <cjk_lang_ja className="vyr" :html="cardData.v_kun.reading"></cjk_lang_ja>
@@ -82,6 +82,45 @@ export default {
 </script>
 
 <style>
+
+/* Kanji flashcard layout */
+.fc-kanji .d-keyword { font:16px Georgia, Times New Roman, serif; position:absolute; left:8px; top:4px; width:204px; }
+.fc-kanji .d-keyword a { text-decoration:none; }
+.fc-kanji .d-keyword a:hover { text-decoration:underline; }
+
+.fc-kanji .d-kanji { position:absolute; left:0px; width:100%; top:40%; margin-top:-50px; text-align:center; }
+.fc-kanji .d-kanji p { margin:0; padding:0; font-size:100pt; line-height:1em; }
+.fc-kanji .d-kanji p img { display:none; }
+
+.fc-kanji .d-strokec { position:absolute; left:8px; bottom:6px; font:12px Georgia, Times New Roman; color:#a0a0a0; }
+.fc-kanji .d-strokec .kanji { font-size:20pt; }
+
+.fc-kanji .d-framenr { font:12px Georgia, Times New Roman; position:absolute; bottom:7px; right:8px; color:#a0a0a0; }
+
+
+/* states :: default review mode */
+.uiFcState-0 .fc-kanji .d-keyword   { color:#000; }
+.uiFcState-0 .fc-kanji .d-kanji     { visibility:hidden; color:#fff; }
+.uiFcState-0 .fc-kanji .d-strokec   { visibility:hidden; }
+.uiFcState-0 .fc-kanji .d-framenr   { visibility:hidden; }
+.uiFcState-0 .fc-kanji .d-yomi      { display:none; } /* vocab */
+
+.uiFcState-1 .fc-kanji .d-keyword   { color:#a0a0a0 }
+.uiFcState-1 .fc-kanji .d-kanji     { visibility:visible; color:#000; }
+.uiFcState-1 .fc-kanji .d-strokec   { visibility:visible; }
+.uiFcState-1 .fc-kanji .d-framenr   { visibility:visible; }
+.uiFcState-1 .fc-kanji .d-yomi      { display:block; } /* vocab */
+
+/* states :: kanji to keyword (explicitly override the defaults above) */
+.uiFcState-0.is-reverse .fc-kanji .d-keyword { visibility:hidden; }
+.uiFcState-0.is-reverse .fc-kanji .d-kanji   { visibility:visible; color:#000; }
+.uiFcState-0.is-reverse .fc-kanji .d-yomi    { display:block; }
+ 
+ /* show kanji compounds on front side, show meanings on back side */
+.uiFcState-0.is-reverse .vyg       { visibility:hidden; }
+.uiFcState-1.is-reverse .vyg       { visibility:visible; }
+
+
 /* Edit Flashcard menu icon */
 #uiFcMenu {
   display:block; position:absolute;
@@ -93,27 +132,43 @@ export default {
 #uiFcMenu:hover, #uiFcMenu.active { background:#e8e8e8; }
 
 /* Onyomi */
-.with-yomi #kanjibig  { width:50%; }
+.with-yomi .d-kanji { width:50%; }
 
-#uiFcYomi        { position:absolute; left:50%; top:0; width:50%; }
-
-#uiFcYomi .yomi  { }
+.d-yomi   { position:absolute; left:50%; top:0; width:50%; }
 
   /* highlight the split reading */
-/*#uiFcYomi .vyc span  { padding-bottom:2px; border-bottom:2px solid #f00; }*/
-#uiFcYomi .vyr em { padding-bottom:2px; border-bottom:2px solid #f00; font-style:normal; }
+.d-yomi .vyr em { padding-bottom:2px; border-bottom:2px solid #f00; font-style:normal; }
 
-#uiFcYomi .pad   { padding:62px 8px 8px; }
+.d-yomi .pad   { padding:62px 8px 8px; }
 
-#uiFcYomi .y_o   { margin:0 0 20px }
+.d-yomi .y_o   { margin:0 0 20px }
 
-#uiFcYomi .cj-k  { line-height:1em; }
-#uiFcYomi .vyc   { font-size:22px; padding:5px 3px; display:inline-block; background:#eee; border-radius:3px;  }
-#uiFcYomi .vyr   { font-size:18px; padding:7px 3px; display:inline-block; margin:0 0 0 1em; }
+.d-yomi .cj-k  { line-height:1em; }
+.d-yomi .vyc   { font-size:22px; padding:5px 3px; display:inline-block; background:#eee; border-radius:3px;  }
+.d-yomi .vyr   { font-size:18px; padding:7px 3px; display:inline-block; margin:0 0 0 1em; }
 
-#uiFcYomi .vyg   { 
+.d-yomi .vyg   { 
   font:italic 15px/1.1em Georgia, serif; color:#888; padding:8px 0 0; 
   /* FIX #81 */
   overflow-y:auto; max-height:6em;
 }
+
+
+/* ================================================================================= */
+/* MOBILE LAYOUT */
+/* ================================================================================= */
+
+@media screen and (max-width:700px) {
+
+  /* Kanji flashcard layout */
+  .d-keyword     { padding:4px 0 0 8px; }
+  .d-kanji       { top:40%; margin-top:-35px; }
+  .d-kanji p     { font-size:100px; }
+
+  .d-yomi .vyc   { font-size:16px; padding:2px 3px; }
+  .d-yomi .vyr   { font-size:14px; padding:3px 3px; }
+  .d-yomi .vyg   { font-size:13px; padding-top:0.5em; }
+
+}
+
 </style>

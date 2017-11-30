674fddb14a03d664db42938e196a780ebd5e734c
Fabrice Denis <fabrice.denis@gmail.com>
2017-11-30 16:39:34 +0100

    Minor update to data/schemas/incremental/rtk_0014_srs_settings.sql

--- data/schemas/incremental/rtk_0014_srs_settings.sql
+++ data/schemas/incremental/rtk_0014_srs_settings.sql
@@ -13,4 +13,4 @@ ALTER TABLE users_settings ADD srs_mult     SMALLINT UNSIGNED NOT NULL;
 ALTER TABLE users_settings ADD srs_hard_box TINYINT  UNSIGNED NOT NULL;
 
 -- update for existing users
-UPDATE users_settings SET srs_max_box = 7, srs_mult = 205, srs_hard_box = 0;
+UPDATE users_settings SET updated_on = updated_on, srs_max_box = 7, srs_mult = 205, srs_hard_box = 0;

506372798e0a3703efcea0a0d8e5442385957db3
Fabrice Denis <fabrice.denis@ipmgroup.be>
2020-03-28 12:21:22 +0100

    refactor(koohiiFormatReading): refactor mixin to an import (wip)

--- src/lib/front/vue/lib/mixins/KoohiiFormat.js
+++ src/lib/front/vue/lib/mixins/KoohiiFormat.js
@@ -1,26 +1,22 @@
 /**
  * Misc. formatting functions.
- * 
- * METHODS
- * 
- *   koohiiformatReading()    Format the highlighted kanji reading from a DictEntry / VocabPick
  *
- * 
+ * METHODS
+ *
+ *   kkFormatReading()
+ *
+ *
  */
 
-export default {
-
-  methods: {
-    /**
-     * Surround the kanji reading with <em> tag
-     *
-     * (cf. data/scripts/dict/dict_gen_cache.php where syntax originates)
-     *
-     * @param {string} kana   compound's reading
-     * @return {string} 
-     */
-    koohiiformatReading(kana) {
-      return kana.replace('(', '<em>').replace(')', '</em>')
-    }
-  }
+/**
+ * Format the highlighted kanji reading from a DictEntry / VocabPick
+ * 
+ *   Surrounds the kanji reading with <em> tag.
+ *   (cf. data/scripts/dict/dict_gen_cache.php where syntax originates)
+ *
+ * @param {string} kana   compound's reading
+ * @return {string}
+ */
+export function kkFormatReading(kana) {
+  return kana.replace("(", "<em>").replace(")", "</em>");
 }

81012c168b787c955a0ef83b2e869c63b43cc421
Fabrice Denis <fabrice.denis@ipmgroup.be>
2021-01-28 16:27:36 +0100

    fix(profile): fix the profile "not found" template
    The "user not found" state was already accounted for, but the template
    was broken.

--- src/apps/koohii/modules/profile/templates/indexError.php
+++ src/apps/koohii/modules/profile/templates/indexError.php
@@ -1,21 +1,18 @@
-<div class="layout-home">
+<?php
+$sf_request->setParameter('_homeFooter', true);
+?>
+<div class="pt-8 markdown">
 
-<?php include_partial('home/homeSide') ?>
+  <h2>Oops! Member Not Found</h2>
 
-<div class="pure-u-1 pure-u-md-3-5">
+  <p>Woops, the user <strong><?php echo esc_specialchars($sf_request->getParameter('username')); ?></strong> could not be found.</p>
 
-  <div class="app-header">
-    <h2>Member Profile</h2>
-    <div class="clear"></div>
-  </div>
+  <p>
+    This user is not in the database. The username could be mistyped. The user may also have deleted their account.
+  </p>
 
-  <p> Sorry, the user <strong><?php echo esc_specialchars($sf_request->getParameter('username')) ?></strong> could not be found.</p>
-
-  <p>What's next:</p>
-  
   <ul>
-      <li><a href="javascript:history.go(-1)">Go back to previous page</a></li>
-      <li><?php echo link_to('Go to Homepage', '@homepage') ?></li>
+    <li><a href="javascript:history.go(-1)">Go back to previous page</a></li>
   </ul>
- 
+
 </div>

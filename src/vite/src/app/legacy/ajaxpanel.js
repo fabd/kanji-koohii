d294ba4a3f9703c1484db2575877245dd9e22d61
fabd <fabrice.denis@gmail.com>
2021-08-04 16:59:21 +0200

    (refactor): remove obsolete "ShadeLayer"
    Should use `KoohiiLoading` instead, or something else. Code is obsolete

--- src/vite/src/app/legacy/ajaxpanel.js
+++ src/vite/src/app/legacy/ajaxpanel.js
@@ -111,171 +111,6 @@ import * as TRON from "@lib/tron";
 
 let AjaxPanel = Core.make();
 
-/**
- * ShadeLayer creates a absolutely positioned div that covers an area of the page,
- * with a solid color and a level of transparency. Typical use is to show an
- * area as "disabled" while a dialog is on, or while content is loading with ajax.
- *
- * Options
- *   element                   If set, the layer is positioned to cover the element's area.
- *   pos
- *     left, top               Must be set if element is not specified.
- *   size
- *     width, height           Must be set if element is not specified.
- *   color                     The solid color to use, in CSS format ('#rgb' or '#rrggbb'). (OPTIONAL)
- *   opacity                   Should be a value from 0 to 100. 0 is invisible, 100 is opaque. (OPTIONAL)
- *   glass_mode                The layer is fully transparent (no color, no opacity) (DEFAULT false)
- *
- * Methods
- *
- *   show()
- *   hide()
- *   resize()           Updates the layer dimensions based on the element (element option must be set)
- *                      This is mostly for when the element is the document body, and the window is resized.
- *   visible()          Returns true if visible
- *   getLayer()         Returns the DIV element created for the shade layer
- *   getDimensions()    Returns dimensions of the element, if element is document.body returns viewport dimensions.
- *
- * Usage
- *
- *   When the layer is created, it is not shown by default, show() must be called.
- *   This allows to change some default values such as zIndex, before displaying the layer.
- *
- */
-
-/**
- * Default settings
- */
-const DEFAULT_COLOR = "#000";
-const DEFAULT_OPACITY = 20;
-/**
- * Currently assumed to be lower value than AjaxIndicator DEFAULT_ZINDEX
- */
-const DEFAULT_ZINDEX = 90;
-
-let ShadeLayer = Core.make();
-
-ShadeLayer.prototype = {
-  init: function (options) {
-    this.color = options.color || DEFAULT_COLOR;
-
-    this.opacity =
-      typeof options.opacity !== "undefined"
-        ? options.opacity
-        : DEFAULT_OPACITY;
-    this.opacity = Math.max(Math.min(this.opacity, 100), 0);
-
-    if (options.element) {
-      this.pos = $$(options.element).offset();
-      this.size = this.getDimensions(options.element);
-
-      this.element = options.element;
-    } else {
-      this.pos = options.pos;
-      this.size = options.size;
-    }
-
-    var elLayer = document.createElement("div");
-    var layerStyles = {
-      display: "none",
-      position: "absolute",
-      top: this.pos.top + "px",
-      left: this.pos.left + "px",
-      zIndex: DEFAULT_ZINDEX,
-      width: /%$/.test(this.size.width)
-        ? this.size.width
-        : this.size.width + "px",
-      height: /%$/.test(this.size.height)
-        ? this.size.height
-        : this.size.height + "px",
-    };
-
-    // set shading
-    if (!options.glass_mode) {
-      // ex: background:#85a5d2; opacity:0.5; filter:alpha(opacity=50);
-      layerStyles.background = this.color;
-      layerStyles.opacity = Math.round(this.opacity) / 100;
-    }
-
-    $$(elLayer).css(layerStyles);
-
-    this.elBody = document.getElementsByTagName("body")[0];
-    this.elBody.insertBefore(elLayer, this.elBody.firstChild);
-    this.elLayer = elLayer;
-  },
-
-  show: function () {
-    // update dimensions (if repeating show/hide and the container content changes)
-    if (this.element) {
-      this.resize();
-    }
-    $$(this.elLayer).css("display", "block");
-  },
-
-  hide: function () {
-    $$(this.elLayer).css("display", "none");
-  },
-
-  resize: function () {
-    if (!this.element) {
-      throw new Error("uiShadeLayer.resize() cannot resize without element");
-    }
-
-    var newSize = this.getDimensions(this.element);
-
-    // try to fix small gap caused by Firefox
-
-    if (
-      newSize.width !== this.size.width ||
-      newSize.height !== this.size.height
-    ) {
-      this.size = newSize;
-      //  uiConsole.log('resize to %o', this.size);
-      $$(this.elLayer).css({
-        width: this.size.width + "px",
-        height: this.size.height + "px",
-      });
-    }
-  },
-
-  visible: function () {
-    return this.elLayer.style.display !== "none";
-  },
-
-  destroy: function () {
-    if (this.elLayer) {
-      this.elBody.removeChild(this.elLayer);
-    }
-    this.elLayer = null;
-  },
-
-  getLayer: function () {
-    return this.elLayer;
-  },
-
-  /**
-   * Wrapper that returns the dimensions of the entire viewport if element is the
-   * document body, or the dimensions of a specific element, calls the appropriate
-   * Prototype function.
-   *
-   * @param {HTMLElement}  element
-   */
-  getDimensions: function (element) {
-    if (element === document.body) {
-      return {
-        width: Dom.getViewportWidth(),
-        height: Dom.getViewportHeight(),
-      };
-    } else {
-      var region = Dom.getRegion(element);
-      return {
-        width: region.width,
-        height: region.height,
-      };
-    }
-  },
-};
-
 AjaxPanel.prototype = {
   options: null,
 
@@ -368,9 +203,6 @@ AjaxPanel.prototype = {
       this.eventDispatcher.notify("onContentDestroy");
     }
 
-    if (this.shadeLayer) {
-      this.shadeLayer.destroy();
-    }
     this.evtCache.destroy();
     this.eventDispatcher.destroy();
   },
@@ -633,21 +465,6 @@ AjaxPanel.prototype = {
   ajaxOnStart: function (eventType, args) {
     //console.log('AjaxPanel.ajaxOnStart(%o)', args);
 
-    // layer
-    if (this.options.bUseLayer) {
-      // create layer, then reuse it
-      if (!this.shadeLayer) {
-        this.shadeLayer = new ShadeLayer({
-          element: this.container,
-          glass_mode: !this.options.bUseShading,
-        });
-      }
-
-      this.shadeLayer.show();
-    } else {
-      this.shadeLayer = null;
-    }
-
     // create a new uiAjaxIndicator because it is added inside the container
     // and the container content can be replaced
     this.ajaxIndicator = new AjaxIndicator({
@@ -669,10 +486,6 @@ AjaxPanel.prototype = {
 
     // hide loading indicator
     this.ajaxIndicator.destroy();
-
-    if (this.shadeLayer) {
-      this.shadeLayer.hide();
-    }
   },
 
   /**

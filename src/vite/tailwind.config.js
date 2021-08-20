0638da4dd58dd15100feeac5711b7224cb78123d
fabd <fabrice.denis@gmail.com>
2021-08-20 11:56:00 +0200

    (tailwind): add path to symfony's global templates for JIT

--- src/vite/tailwind.config.js
+++ src/vite/tailwind.config.js
@@ -2,9 +2,12 @@ module.exports = {
   mode: "jit",
   purge: [
     "./src/**/*.{js,ts,vue}",
+
+    // Symfony's global templates (including global layout)
+    "../apps/koohii/templates/*.php",
+
+    // Symfony's per module/action templates
     "../apps/koohii/modules/**/templates/*.php",
-    //
-    //"./src/**/*.{js,jsx,ts,tsx,vue}",
   ],
 
   theme: {

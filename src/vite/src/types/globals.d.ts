fe07e1698afa771a0e0d87a267d056c3d7791dcb
fabd <fabrice.denis@gmail.com>
2021-09-01 17:11:05 +0200

    Fix globals.d.ts as ambient types since removing the imports

--- src/vite/src/types/globals.d.ts
+++ src/vite/src/types/globals.d.ts
@@ -7,45 +7,46 @@
  *
  */
 
-declare global {
-  interface Window {
-    // cf. kk_globals_put() on the php side
-    KK: {
-      // base URL for API requests (cf. layout.php & koohii_base_url() helper)
-      BASE_URL: string;
+interface Window {
+  // cf. kk_globals_put() on the php side
+  KK: {
+    // base URL for API requests (cf. layout.php & koohii_base_url() helper)
+    BASE_URL: string;
 
-      // site-wide mobile nav, setup data from php with the correct urls
-      MBL_NAV_DATA: Dictionary;
+    // site-wide mobile nav, setup data from php with the correct urls
+    MBL_NAV_DATA: Dictionary;
 
-      STUDY_SEARCH_URL: string;
+    STUDY_SEARCH_URL: string;
 
-      EDITSTORY_PROPS: Dictionary;
+    EDITSTORY_PROPS: Dictionary;
 
-      REVIEW_OPTIONS: Dictionary;
-      REVIEW_MODE: Dictionary;
+    REVIEW_OPTIONS: Dictionary;
+    REVIEW_MODE: Dictionary;
 
-      // study > My Stories
-      MYSTORIES_SORT_ACTIVE: string;
-      MYSTORIES_SORT_OPTIONS: Dictionary;
+    // study > My Stories
+    MYSTORIES_SORT_ACTIVE: string;
+    MYSTORIES_SORT_OPTIONS: Dictionary;
 
-      ACCOUNT_SRS: Dictionary;
+    ACCOUNT_SRS: Dictionary;
+
+    // Kanji Recognition
+    READING_KEYWORDS: Dictionary;
+  };
+
+  // 4th/5th edition keywords and kanji, import cf. _SideColumn.php
+  //   web/revtk/study/keywords-rtk-0.js
+  //   web/revtk/study/keywords-rtk-1.js
+  kklist: string;
+  kwlist: string[];
+
+  Koohii: {
+    // misc. references shared between backend/frontend,
+    //  also Vue components from Vite build, instanced from php templates
+    Refs: {
+      [key: string]: any;
     };
 
-    // 4th/5th edition keywords and kanji, import cf. _SideColumn.php
-    //   web/revtk/study/keywords-rtk-0.js
-    //   web/revtk/study/keywords-rtk-1.js
-    kklist: string;
-    kwlist: string[];
-
-    Koohii: {
-      // misc. references shared between backend/frontend,
-      //  also Vue components from Vite build, instanced from php templates
-      Refs: {
-        [key: string]: any;
-      };
-
-      // references to Vue components that can be instanced later
-      UX: { [componentName: string]: any };
-    };
-  }
+    // references to Vue components that can be instanced later
+    UX: { [componentName: string]: any };
+  };
 }

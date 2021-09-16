994f344d052125f9a82c39a85afdc3b9b57e8801
fabd <fabrice.denis@gmail.com>
2021-09-16 12:18:43 +0200

    (eslintrc): no-empty, no-unused-vars

--- src/vite/.eslintrc.js
+++ src/vite/.eslintrc.js
@@ -24,11 +24,16 @@ module.exports = {
     // we're stripping console.* calls in production build
     "no-console": "off",
 
+    // reduce distractions when editing code
+    "no-empty": "warn",
+    "@typescript-eslint/no-empty-function": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+
     // this check is already covered by TS (Vetur), complains about types
     // declared in globals.d.ts
     "no-undef": "off",
 
-    "no-unused-vars": "off",
     "vue/max-attributes-per-line": "off",
     "vue/no-v-html": "off",
     // legacy js

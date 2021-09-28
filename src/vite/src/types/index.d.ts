eb1736c67c62636ed05af7c0ffe934fa91c127ba
fabd <fabrice.denis@gmail.com>
2021-09-28 12:07:32 +0200

    (refactor): remove obsolete interface
    SelectionTable was refactor to a native JS class

--- src/vite/src/types/index.d.ts
+++ src/vite/src/types/index.d.ts
@@ -1,11 +1,1 @@
 type Dictionary<T = any> = { [key: string]: T };
-
-// --------------------------------------------------------------------
-// legacy component definitions; for .js files imported in .ts
-// --------------------------------------------------------------------
-
-interface ISelectionTable {
-  new (container: string | Element): this;
-  destroy(): void;
-  getPostData(): Dictionary;
-}

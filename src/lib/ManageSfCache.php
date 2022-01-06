832134eb1963074c80c2696d3abb8462b3af0f86
fabd <fabrice.denis@gmail.com>
2022-01-06 17:06:56 +0100

    (ManageSfCache): handle single module/action cache file, php formatting

--- src/lib/ManageSfCache.php
+++ src/lib/ManageSfCache.php
@@ -3,22 +3,27 @@
  * ManageSfCache provides utility methods to manage Symfony's cache of
  * actions and partials.
  *
+ * Static methods:
+ *
+ *   clearCacheWildcard()      ... clear all cache for given action/module
+ *
+ *
  * CLEARING CACHED TEMPLATES
  *
  *   $path = ManageSfCache::getRealPathForCache('module/action');
  *   ManageSfCache::recursiveDeleteFromPath($path);
- *   
  */
-
 class ManageSfCache
 {
-  const APP_NAME = 'koohii';
+  public const APP_NAME = 'koohii';
 
   /**
    * Clear several cache parts at once using wildcard cache key (sf 1.1).
    *
    * Remember: for partials and components, action is "_PartialName".
-   * 
+   *
+   * @param mixed $module
+   * @param mixed $action
    */
   public static function clearCacheWildcard($module, $action)
   {
@@ -27,50 +32,66 @@ class ManageSfCache
   }
 
   /**
-   * Return the root path used by symfony for all cached templates of action or _partial
-   * 
+   * Return the root path used by symfony for all cached templates of action or _partial.
+   *
+   * @return false|string
    */
-  public static function getRealPathForCache($route)
+  public static function getRealPathForCache(string $route)
   {
-
-    list($module, $action) = self::getModuleActionFromRoute($route);
+    [$module, $action] = self::getModuleActionFromRoute($route);
+    // LOG::info("module action = $module $action");
 
     $dir = sfConfig::get('sf_cache_dir').'/'.self::getAppSlashEnv().'/template/'.self::getHostName().'/all/';
-    
-    if (substr($action, 0 , 1) === '_') {
+
+    if (substr($action, 0, 1) === '_')
+    {
       $dir = $dir.'sf_cache_partial/'.$module.'/_'.$action.'/sf_cache_key/';
     }
-    else {
-      $dir = $dir.$module.'/'.$action.'/';
+    else
+    {
+      $dir = $dir.$module.'/'.$action;
+
+      if (is_file("{$dir}.cache"))
+      {
+        $dir = "{$dir}.cache"; // simple action with no params
+      }
+      else
+      {
+        $dir = $dir.'/';
+      }
     }
 
-  // dbg::out($dir);
+    // LOG::info('dir', $dir);
 
-    // real path or false
     return realpath($dir);
   }
 
-  // delete all files and subfolders in path (but not the path itself)
-  public static function recursiveDeleteFromPath($path, $level = 0)
+  /**
+   * Delete all files and subfolders in path (but not the path itself).
+   *
+   * @param mixed $level
+   */
+  public static function recursiveDeleteFromPath(string $path, $level = 0)
   {
-  //  echo "<br>$path --- level $level";
-    if (is_dir($path) === true)
+    if (is_dir($path))
     {
       $files = array_diff(scandir($path), ['.', '..']);
 
       foreach ($files as $file)
       {
-        self::recursiveDeleteFromPath(realpath($path) . '/' . $file, $level + 1);
+        self::recursiveDeleteFromPath(realpath($path).'/'.$file, $level + 1);
       }
 
-      if ($level > 0) {
+      if ($level > 0)
+      {
         return rmdir($path);
       }
-      else {
+      else
+      {
         return true;
       }
     }
-    else if (is_file($path) === true)
+    elseif (is_file($path))
     {
       return unlink($path);
     }
@@ -78,49 +99,78 @@ class ManageSfCache
     return false;
   }
 
-  // return file count and total size for all files (recursively) in cache directory for module/action (or module/_partial)
+  /**
+   * Return file count and total size for all files (recursively)
+   *  in cache directory for module/action (or module/_partial).
+   *
+   * @param mixed $route
+   *
+   * @return array
+   */
   public static function getCacheSizeInfo($route)
   {
     $real_path = ManageSfCache::getRealPathForCache($route);
+    // LOG::info('route', $real_path);
 
-    if (false === $real_path) {
-      return ['real_path' => '-INVALID PATH-', 'file_count' => -1, 'size_kb' => -1];
+    if (false === $real_path)
+    {
+      return ['real_path' => '-NO CACHE FILE(s)-', 'file_count' => -1, 'size_kb' => -1];
     }
 
     $size = 0;
 
-    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($real_path, FilesystemIterator::SKIP_DOTS));
-    foreach ($iterator as $i) {
-      $size += $i->getSize();
+    if (is_dir($real_path))
+    {
+      $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($real_path, FilesystemIterator::SKIP_DOTS));
+      foreach ($iterator as $i)
+      {
+        $size += $i->getSize();
+      }
+      $file_count = iterator_count($iterator);
+    }
+    else
+    {
+      $size = filesize($real_path);
+      $file_count = 1;
     }
 
     //echo sprintf('%d size, %d files', $size, iterator_count($iterator));
 
     return [
-      'real_path'   => $real_path,
-      'file_count'  => iterator_count($iterator),
-      'size_kb'     => (int) ($size / 1024),
+      'real_path' => $real_path,
+      'file_count' => $file_count,
+      'size_kb' => (int) ($size / 1024),
     ];
   }
 
-  // returns string 'application/environment' (part of the cache folder path)
-  public static function getAppSlashEnv()
+  /**
+   * Returns 'application/environment' (part of the cache folder path).
+   */
+  public static function getAppSlashEnv(): string
   {
     $env = strtolower(sfApplicationConfiguration::getActive()->getEnvironment());
+
     return self::APP_NAME.'/'.$env;
   }
 
   // returns array('module', 'action') for given $route
   public static function getModuleActionFromRoute($route)
   {
-    list($module, $action) = explode('/', $route);
+    [$module, $action] = explode('/', $route);
+
     return [$module, $action];
   }
 
-  // Format host name as used by Symfony in cache path name
+  /**
+   * Format host name as used by Symfony1.x in cache path name.
+   *
+   * In development it returns `localhost`
+   *
+   * In production it looks like `kanji_koohii_com`
+   */
   public static function getHostName()
   {
-    $context  = sfContext::getInstance();
+    $context = sfContext::getInstance();
     $hostName = $context->getRequest()->getHost();
 
     $hostName = preg_replace('/[^a-z0-9\*]/i', '_', $hostName);

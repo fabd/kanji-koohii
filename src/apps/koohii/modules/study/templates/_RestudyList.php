d86792f62679ab7f56ef36ba5b94676636212e62
fabd <fabrice.denis@gmail.com>
2022-03-25 17:00:55 +0100

    (Restudy List): implement sidebar "Restudy" pane, delete old css

--- src/apps/koohii/modules/study/templates/_RestudyList.php
+++ src/apps/koohii/modules/study/templates/_RestudyList.php
@@ -1,17 +1,13 @@
-<?php 
-  // $ucsId    = ReviewsPeer::getNextUnlearnedKanji($sf_user->getUserId());
-  // $kanji    = KanjisPeer::getKanjiByUCS($ucsId);
-  // $next_uri = 'study/edit?id='.$kanji->kanji;
- ?>
-
-<div id="study-restudy" class="study-action-comp no-gutter-xs-sm dsk:mb-4">
-  <div class="mbl:flex items-center">
-
-    <h3 class="">Restudy <strong><?php echo $restudy_count ?></strong></h3>
-
-    <div class="mbl:ml-auto">
-      <?php echo link_to('Start', 'study/edit', ['query_string' => "restudy", 'class' => 'ko-Btn is-ghost']) ?>
-    </div>
-
-  </div>
+<?php
+?>
+<div class="ko-Box ko-Box--danger ko-Box--stroke no-gutter-xs-sm dsk:mb-4">
+  <h3 class="text-danger-dark font-bold leading-1 mb-2">Restudy</h3>
+  <p class="text-danger-darker text-sm mb-2">
+    <strong><?= $restudyCount; ?></strong> Forgotten Kanji
+  </p>
+<?= _bs_button(
+  'Begin Restudy<i class="fa fa-book-open ml-2"></i>',
+  'study/edit',
+  ['query_string' => 'restudy', 'class' => 'ko-Btn ko-Btn--danger']
+); ?>
 </div>

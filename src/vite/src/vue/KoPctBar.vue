8ea033705906a0c00af2ec1a4136b5ad9f81c3cd
fabd <fabrice.denis@gmail.com>
2022-04-01 15:34:48 +0200

    Add `PctBar` component + /ux demo

---
+++ src/vite/src/vue/KoPctBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="ko-PctBar">
+      <transition name="chart-fade" appear>
+        <div
+          class="ko-PctBar-fill"
+          :style="{
+            'min-width': cssFillWidth
+          }"
+        >{{ pctValue }}%</div>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "KoPctBar",
+
+  props: {
+    value: { type: Number, required: true },
+    maxValue: { type: Number, required: true },
+  },
+
+  computed: {
+    cssFillWidth(): string {
+      return `${this.pctValue}%`;
+    },
+
+    pctValue(): number {
+      let pct = (this.value * 100) / this.maxValue;
+      let floor = Math.floor(pct);
+
+      return Math.max(floor, 1);
+    },
+  },
+});
+</script>

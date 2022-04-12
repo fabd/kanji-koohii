3f959053117fd1621d29c41ee34a25deaf957a91
fabd <fabrice.denis@gmail.com>
2022-04-12 12:07:51 +0200

    (KoKanjiCard): wrap the kanji with the cjk lang attr. component

--- src/vite/src/vue/KoKanjiCard.vue
+++ src/vite/src/vue/KoKanjiCard.vue
@@ -2,7 +2,9 @@
   <div class="ko-KanjiCard">
     <div class="ko-KanjiCard-idx">{{ card.id }}</div>
     <div class="ko-KanjiCard-key">{{ 'keyword' }}</div>
-    <div class="ko-KanjiCard-chr">{{ String.fromCodePoint(card.id) }}</div>
+    <div class="ko-KanjiCard-chr">
+      <cjk-lang-ja>{{ String.fromCodePoint(card.id) }}</cjk-lang-ja>
+    </div>
     <div class="ko-KanjiCard-tag">
       <span>{{ card.tag }}</span>
     </div>
@@ -12,6 +14,8 @@
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 
+import CjkLangJa from "@/vue/CjkLangJa.vue";
+
 export type TKanjiCardData = {
   id: TUcsId;
   tag: string;
@@ -19,6 +23,10 @@ export type TKanjiCardData = {
 
 export default defineComponent({
   name: "KoKanjiCard",
+
+  components: {
+    CjkLangJa,
+  },
 
   props: {
     card: { type: Object as PropType<TKanjiCardData>, required: true },

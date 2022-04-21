a8856f76bb71f14e2358ffde4f57dd4af714df56
fabd <fabrice.denis@gmail.com>
2022-04-21 12:22:13 +0200

    Add `<ko-ratio-box>` component

---
+++ src/vite/src/vue/KoRatioBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ko-RatioBox">
+    <div class="ko-RatioBox-height">
+      <div :class="['ko-RatioBox-inner', inner]">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "KoRatioBox",
+
+  props: {
+    inner: { type: String, required: false, default: '' },
+  },
+
+});
+</script>

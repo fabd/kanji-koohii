1f35519fd6ea1be13304a7b7f2fa8e62579fa223
fabd <fabrice.denis@gmail.com>
2022-04-22 12:35:43 +0200

    (misc/reading): update Kanji Recognition to use optimized keywords data

--- src/apps/koohii/modules/misc/actions/actions.class.php
+++ src/apps/koohii/modules/misc/actions/actions.class.php
@@ -6,15 +6,28 @@ class miscActions extends sfActions
   {
   }
 
+  /**
+   * Note: similar to the View All Lessons page, we include the original
+   * RTK keywords and kanjis as a javascript file, which is (in theory) more
+   * efficient than pulling them from the db (otherwise the full coalesced
+   * custom > orig keywords map is ~60 KB in the output html - before gzip).
+   *
+   * It is the same file that is already included on the Study pages anyway
+   * and therefore likely in the browser's cache.
+   *
+   * We pull the user's customized keywords into a separate map, which is
+   * a simpler query - and assuming most users don't customize many keywords.
+   *
+   * The helpers on the frontend side "coalesce" the custom keywords with
+   * the original keywords, for any kanji in the sequence.
+   *
+   * @param coreRequest $request
+   */
   public function executeReading($request)
   {
     $userId = $this->getContext()->getUser()->getUserId();
-    $keywords = CustkeywordsPeer::getCoalescedKeywords($userId);
 
-    $keywordsMap = array_map(
-      fn ($item) => [(int) $item['ucs_id'], $item['keyword']],
-      array_values($keywords)
-    );
+    $keywordsMap = CustkeywordsPeer::getUserKeywordsMap($userId);
 
     $knownKanji = ReviewsPeer::getKnownKanji($userId);
 
@@ -22,7 +35,7 @@ class miscActions extends sfActions
     kk_globals_put('USER_KEYWORDS_MAP', $keywordsMap);
     kk_globals_put('USER_KNOWN_KANJI', $knownKanji);
 
-    // output SEQ_KANJIS for rtk.ts helpers
+    // include RTK keywords and kanjis (cf. rtk.ts helpers)
     rtkIndex::useKeywordsFile();
   }
 }

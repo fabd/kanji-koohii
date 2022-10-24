16be423596412bcbee6d0ab0e27d01b20b480d8d
fabd <fabrice.denis@gmail.com>
2022-10-24 12:06:14 +0200

    Update intro paragraph & help links in SRS settings page, minor formatting

--- src/apps/koohii/modules/account/templates/spacedrepetitionSuccess.php
+++ src/apps/koohii/modules/account/templates/spacedrepetitionSuccess.php
@@ -1,25 +1,28 @@
 <?php use_helper('Form', 'Validation', 'Decorator'); ?>
 
-<?php decorate_start('SideTabs', ['active' => 'spacedrepetition']) ?>
+<?php decorate_start('SideTabs', ['active' => 'spacedrepetition']); ?>
 
   <h2>Spaced Repetition Settings</h2>
 
   <p>
-    See <?= link_to('documentation', '@learnmore#srs') ?> for Kanji Koohii's implementation of the Leitner SRS.
-  </p
-  <p>
-    Also see <?= link_to('tips for rating cards', '@learnmore#faq') ?> in the F.A.Q.
+    Learn more about Koohii’s <?= link_to('Leitner SRS implementation', '@learnmore#srs'); ?> in the documentation.
   </p>
 
-  <?php echo form_errors() ?>
-  <?php echo form_tag('account/spacedrepetition') ?>
+  <p>
+    Also see the <b>tips</b> in the <?= link_to('Card Ratings', '@learnmore#rating'); ?> section.
+  </p>
+
+  <div class="pb-4"></div>
+
+  <?= form_errors(); ?>
+  <?= form_tag('account/spacedrepetition'); ?>
     <div id="JsVueMountSrsForm"></div>
     <p>
-      <?php echo submit_tag('Save changes', ['class' => 'ko-Btn ko-Btn--success']) ?>
+      <?= submit_tag('Save changes', ['class' => 'ko-Btn ko-Btn--success']); ?>
     </p>
   </form>
 
-<?php decorate_end() ?>
+<?php decorate_end(); ?>
 
 <?php
   kk_globals_put('ACCOUNT_SRS', $srsSettings);

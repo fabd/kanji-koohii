cddf16f36c858aa9bd0a656383d29a2138a8fdc2
fabd <fabrice.denis@gmail.com>
2023-04-06 17:40:17 +0200

    (flashcardlist): minor tweaks using responsive font size

--- src/apps/koohii/modules/manage/templates/flashcardlistSuccess.php
+++ src/apps/koohii/modules/manage/templates/flashcardlistSuccess.php
@@ -4,15 +4,17 @@
 
   <div class="row mb-8">
     <div class="col-lg-6">
+      <p class="ux-text-md">
       Click a column heading to sort
       the table on that column, click more than once to revert the sort order.
       Note that in addition to the column you selected, there is always a secondary
       sorting on the frame number.
+      </p>
     </div>
 
     <div class="col-lg-6">
       <div class="ko-Box">
-        <h3 class="text-md font-bold">Statistics</h3>
+        <h3 class="font-bold">Statistics</h3>
         <strong><?= ReviewsPeer::getFlashcardCount($sf_user->getUserId()); ?></strong> flashcards.<br />
       </div>
     </div>

02be34c270af2203ed5711e67e157d7a3d3d2557
fabd <fabrice.denis@gmail.com>
2024-04-05 14:35:20 +0200

    (forgotPassword): fix the "MailSent" template, remove old sidebar

--- src/apps/koohii/modules/account/templates/forgotPasswordMailSent.php
+++ src/apps/koohii/modules/account/templates/forgotPasswordMailSent.php
@@ -1,18 +1,18 @@
-<div class="">
+<?php
+  $sf_user_name = $sf_user->getUserName();
+?>
 
-<?php include_partial('home/homeSide') ?>
+<h2>New Password Confirmation</h2>
 
-<div class="pure-u-1 pure-u-md-3-5">
-  
-
-    <h2>New Password Confirmation</h2>
-
-    <p>  A new password was sent to the email address that you used
-      during registration on this website.
-    </p>
-    <p> The email should arrive  shortly. Once you have your new
-      password, proceed to the <?php echo link_to('login page','@login') ?>.
-    </p>
-
+<p>
+  A new password was sent to the email address that you used
+  during registration on this website.
+</p>
+<p>
+  The email should arrive  shortly.
+</p>
+<p>
+  <?php echo _bs_button_to('Sign in','@login', ['query_string' => 'username='.$sf_user_name, 'class' => 'ko-Btn ko-Btn--success ko-Btn--large mr-2']) ?> with your new password.
+</p>
  
-</div>
+      

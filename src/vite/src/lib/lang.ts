d504db160c8ec9e8164b5c0e3c3f87b2af794dae
fabd <fabrice.denis@gmail.com>
2026-01-20 21:21:42 +0100

    (lang.ts) fixes

--- src/vite/src/lib/lang.ts
+++ src/vite/src/lib/lang.ts
@@ -1,14 +1,14 @@
 export default {
-  isArray: (o: any): boolean => Array.isArray(o),
+  isArray: (o: any): o is any[] => Array.isArray(o),
   isBoolean: (o: any): o is boolean => typeof o === "boolean",
   isFunction: (f: unknown): f is AnyFunction => typeof f === "function",
   isNumber: (s: any): s is number => typeof s === "number",
   isNull: (o: any): o is null => o === null,
-  isObject: (o: any): o is object => typeof o === "object",
+  isObject: (o: any): o is object => o !== null && typeof o === "object",
   isString: (s: any): s is string => typeof s === "string",
   isUndefined: (o: any): o is undefined => typeof o === "undefined",
 
   // DOM
-  isNode: (el: any): boolean => el instanceof Node,
+  isNode: (el: any): el is Node => el instanceof Node,
   isWindow: (o: any): o is Window => o === window,
 };

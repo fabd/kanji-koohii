81adfe766d8b1af1d0ef574b2785150f9cf5212c
fabd <fabrice.denis@gmail.com>
2026-02-04 17:52:46 +0100

    (eslint): fix for shims-vue.d.ts

--- src/vite/src/shims-vue.d.ts
+++ src/vite/src/shims-vue.d.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+
 declare module "*.vue" {
-  import { DefineComponent } from "vue";
+  import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }

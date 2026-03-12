09f668f36665d13af7ea22bf0c5ba2266d07a824
fabd <fabrice.denis@gmail.com>
2026-03-12 18:59:11 +0100

    (main.build.css): news-by-month css tidy up

--- src/apps/koohii/modules/news/templates/_archiveList.php
+++ src/apps/koohii/modules/news/templates/_archiveList.php
@@ -5,13 +5,11 @@
 
     <h2>By Month</h2>
 
-  <div id="news-by-month">
-    <ul>
+    <ul class="ko-NewsByMonth max-lg:flex flex-wrap max-md:text-sm">
 <?php
   foreach (SitenewsPeer::getArchiveIndex() as $p) {
 //  DBG::printr($p);exit;
     $label = simple_format_date(mktime(0,0,0,$p->month,1,$p->year), "M Y") . ' <span>('.$p->count.')</span>';
-    echo '<li>'.link_to($label, 'news/index?year='.$p->year.'&month='.$p->month).'</li>'."\n";
+    echo '<li class="max-lg:w-1/3">'.link_to($label, 'news/index?year='.$p->year.'&month='.$p->month).'</li>'."\n";
   } ?>
     </ul>
-  </div>
